4fa08fec157865aba5f6575d759caac80d429387
Anh Cao <anhcao563@gmail.com>
2021-12-27 11:40:29 +0200

    Add .gitignore

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
-
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
@@ -15,7 +13,7 @@ weatherForm.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location + "'").then((response) => {
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 messageOne.textContent = data.error
